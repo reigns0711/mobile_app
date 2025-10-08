a68fa5339085c5df437214720fdabb2324a24675
Lokesh <Reigns_07>
2025-10-08 13:03:42 +0530

    React mobile applications init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-[red] text-3xl font-bold">Welcome to movie</Text>
+    </View>
+  );
+}
